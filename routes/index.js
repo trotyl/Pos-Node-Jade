159cee7099f0565db504183600271e5e4b012cc0
Trotyl Yu <yzj1995@vip.qq.com>
2014-09-26 18:49:20 +0800

    实现购物车页静态模版

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,12 @@ router.get('/list', function(req, res) {
     res.render('list', { title: '商品列表' , active: {list: true}, list: fixtures.loadAllItems()});
 });
 
+router.get('/cart', function(req, res) {
+    res.render('cart', { title: '购物车' , active: {cart: true}, list: [], total: 0});
+});
+
+router.get('/payment', function(req, res) {
+    res.render('payment', { title: '付款页' , active: {}});
+});
+
 module.exports = router;
