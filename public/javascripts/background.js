a0f45ed7276d5b5752ef0bd30c6780c8b516d3b2
Trotyl Yu <yzj1995@vip.qq.com>
2014-09-29 15:44:39 +0800

    修改Admin页加减按钮BUG

--- public/javascripts/background.js
+++ public/javascripts/background.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     $('#item-manage-list').on('click', 'button', function () {
-        var item_name = $(self).closest('tr').children().first().next().text();
+        var item_name = $(this).closest('tr').children().first().next().text();
         var operation = { '+': 'add', '-': 'minus'}[$(this).text()];
-        var number = $(self).closest('.btn-group').find('.item-count');
-        var count = parseInt(number.text());
+        var number = $(this).closest('.btn-group').find('.item-count');
+        var count = parseInt(number.val());
         if (operation == 'add') {
-            number.text(count + 1);
+            number.val(count + 1);
         }
-        else if (parseInt(number.text()) > 0) {
-            number.text(count - 1);
+        else if (count > 0) {
+            number.val(count - 1);
         }
         else {
             return;
