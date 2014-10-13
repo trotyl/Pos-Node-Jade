9d67c7f0b21e1401d91bcd63f54e277f975fcb40
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-13 11:44:42 +0800

    更改页面加载逻辑

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var moment = require('moment');
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: '首页' , view: 'home', active: {home: true}});
+    res.render('index', { title: '首页' , view: 'home', active: {home: true}});
 });
 
 router.get('/list', function(req, res) {
@@ -16,37 +16,11 @@ router.get('/list', function(req, res) {
 });
 
 router.get('/cart', function(req, res) {
-    Order.all(function (err, list) {
-        if (err) {
-            console.log(err);
-        }
-        var cartStats = Order.getCartStats(list);
-        var items = _(list).filter(function (record) {
-            return record.count > 0;
-        }).value();
-        res.render('cart', { title: '购物车' , view: 'cart', active: {cart: true}, items: items, total: cartStats.total});
-    });
+    res.render('loading', { title: '购物车' , view: 'cart', active: {cart: true}});
 });
 
 router.get('/payment', function(req, res) {
-    Order.all(function (err, list) {
-        if (err) {
-            console.log(err);
-        }
-        var cartStats = Order.getCartStats(list);
-        var bought_items = _(list).filter(function (record) {
-            return record.count > 0;
-        }).value();
-        var free_items = _(bought_items).filter(function (record) {
-            return record.promotion && record.free() > 0;
-        }).value();
-        if (cartStats.count <= 0) {
-            return res.redirect('/list');
-        }
-        res.render('payment', { title: '付款页', view: 'payment', active: {}, bought_items: bought_items, free_items: free_items,
-            total: cartStats.total, saving: cartStats.saving, time: moment().format('YYYY年MM月DD日 HH:mm:ss')
-        });
-    });
+    res.render('loading', { title: '付款', view: 'payment', active: {}})
 });
 
 module.exports = router;
