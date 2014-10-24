79e805adae4bcffdcb7f5cbd75a384d644397857
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-24 15:47:22 +0800

    引入 bower

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', routes);
 app.use('/api', api);
