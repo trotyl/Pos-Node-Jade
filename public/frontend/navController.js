38df8137ce0de7207e7b283e2edd7861ae03fee5
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-25 19:10:33 +0800

    增加前端控制器 前台首页

--- public/frontend/navController.js
+++ public/frontend/navController.js
@@ -2,11 +2,28 @@ posManager.controller('NavController', ['$scope', '$location', '$route', '$route
     function($scope, $location, $route, $routeParams, Item) {
 
         var initialize = function () {
-
+            var path = $location.path();
+            $scope.active = {
+                home: path === '/',
+                list: path === '/list',
+                cart: path === '/cart',
+                payment: path === '/payment'
+            };
         };
         initialize();
 
+        $scope.goHome = function () {
+            $location.path('/');
+        };
 
+        $scope.goToList = function () {
+            $location.path('/list');
+        };
+
+        $scope.goToCart = function () {
+            $location.path('/cart');
+        };
+        
     }]);
 
 
