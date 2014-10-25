c75af7585d9da8727b65e44a6397469f9578c64c
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-25 18:47:11 +0800

    删除不必要的 js 和 css文件

--- public/javascripts/controller/ListController.js
+++
@@ -1,6 +0,0 @@
-function ListController () {
-    $('.list-item').on('click', 'button', function () {
-        var itemName = $(this).closest('.list-item').find('.item-name').text();
-        Cart.add(itemName);
-    });
-}
