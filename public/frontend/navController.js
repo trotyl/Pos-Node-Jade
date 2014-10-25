0142f8cb6dc0301b7c5ab5b3e4eb90c9ebd1a05b
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-25 19:06:34 +0800

    修改后台模版页 POS机

---
+++ public/frontend/navController.js
@@ -0,0 +1,12 @@
+posManager.controller('NavController', ['$scope', '$location', '$route', '$routeParams', 'Item',
+    function($scope, $location, $route, $routeParams, Item) {
+
+        var initialize = function () {
+
+        };
+        initialize();
+
+
+    }]);
+
+
