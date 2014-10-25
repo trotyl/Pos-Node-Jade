aea628557febd575911d80787d3ca93b0addda4a
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-25 20:02:47 +0800

    删除不必要的后台路由

--- routes/admin.js
+++ routes/admin.js
@@ -3,46 +3,7 @@ var router = express.Router();
 var Storage = require('../models/storage');
 
 router.get('/', function(req, res) {
-    res.render('admin', { title:'商品信息管理', view: 'index', page: 1});
-});
-
-router.get('/page/:id', function(req, res) {
-    var page = req.param('id');
-    res.render('admin', { title:'商品信息管理', view: 'index', page: page});
-});
-
-router.get('/detail/:name', function (req, res) {
-    var name = req.param('name');
-    Storage.getItem(name, function (err, result) {
-        res.render('admin/detail', { title: '商品详情', view: 'detail', item: result});
-    });
-});
-
-router.get('/create', function (req, res) {
-    res.render('admin/create', { title: '添加商品', view: 'create'});
-});
-
-router.get('/attribute', function (req, res) {
-    var from = req.param('from');
-    res.render('admin/attribute', { title: '添加属性', view: 'add', from: from});
-});
-
-router.get('/attribute/:name', function (req, res) {
-    var name = req.param('name');
-    var from = req.param('from');
-    res.render('admin/attribute', { title: '添加属性', view: 'add', from: from, name: name});
-});
-
-router.get('/remove', function (req, res) {
-    res.render('admin/remove', { title: '添加属性', view: 'remove', from: 'create'});
-});
-
-router.get('/remove/:name', function (req, res) {
-    var name = req.param('name');
-    var from = req.param('from');
-    Storage.getItem(name, function (err, result) {
-        res.render('admin/remove', { title: '添加属性', view: 'remove', item: result, from: from});
-    });
+    res.render('admin', { title:'商品信息管理' });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,7 @@ var moment = require('moment');
 /* GET home page. */
 
 router.get('/', function(req, res) {
-    res.render('index', { title: '首页' , view: 'home', active: {home: true}});
-});
-
-router.get('/list', function(req, res) {
-    Storage.allItems(function (err, result) {
-        res.render('list', { title: '商品列表' , view: 'list', active: {list: true}, list: result});
-    });
-});
-
-router.get('/cart', function(req, res) {
-    res.render('loading', { title: '购物车' , view: 'cart', active: {cart: true}});
-});
-
-router.get('/payment', function(req, res) {
-    res.render('loading', { title: '付款', view: 'payment', active: {}})
+    res.render('index', { title: '首页' });
 });
 
 module.exports = router;
