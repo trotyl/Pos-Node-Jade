cbe0d03ac1b0a4ef6e569682f48457aa17b53fd0
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-25 16:42:36 +0800

    增加前端单元测试 后台添加商品属性页2

--- test/unit/addAttributeControllerSpec.js
+++ test/unit/addAttributeControllerSpec.js
@@ -56,69 +56,49 @@ describe('PosManager ', function() {
 
     });
 
-    xdescribe('EditController of Editing', function(){
+    describe('AddAttributeController of Editing', function(){
         var scope, ctrl, $httpBackend, location;
-        var item = {
-            id: 'ITEM0001',
-            name: '可乐',
-            unit: '瓶',
-            price: 3,
-            type: '饮料',
-            amount: 100,
-            attrs: [
-                {
-                    name: '属性A',
-                    val: '值A',
-                    birth: null
-                },
-                {
-                    name: '属性B',
-                    val: '值B',
-                    birth: null
-                }
-            ],
-            birth: new Date
-        }
+
 
         beforeEach(inject(function(_$httpBackend_, $rootScope, $location, $route, $routeParams, $controller) {
+
             $httpBackend = _$httpBackend_;
 
-            $httpBackend.whenGET('/backend/edit/edit.html').respond('');
-            $httpBackend.expectGET('/api/item/ITEM0001').respond(item);
+            $httpBackend.whenGET('/api/item/ITEM0001').respond({
+                id: 'ITEM0001',
+                attrs: []
+            });
+            $httpBackend.whenGET('/backend/addAttribute/addAttribute.html').respond('');
 
             scope = $rootScope.$new();
             location = $location;
-            location.path('/detail/ITEM0001');
-            ctrl = $controller('EditController', {$scope: scope, $routeParams: { itemId: 'ITEM0001'}});
+            location.path('/add_attribute/ITEM0001');
+            ctrl = $controller('AddAttributeController', {$scope: scope, $routeParams: { itemId: 'ITEM0001'}});
         }));
 
-
-        it('should know is now in editing mode', function() {
-            $httpBackend.flush();
-            expect(scope.item.id).toBe('ITEM0001');
+        it('should know is now in creating mode', function() {
+            expect(scope.isNew).toBe(false);
         });
 
-        it('should be able to save item', function () {
-            $httpBackend.expectPOST('/api/item/ITEM0001');
-            scope.saveIt();
-        });
-
-        it('should be able to add attribute', function () {
-            $httpBackend.flush();
-            scope.addAttr();
-            expect(location.path()).toBe('/add_attribute/ITEM0001');
-        });
-
-        it('should be able to remove attribute', function () {
-            $httpBackend.flush();
-            scope.removeAttr();
-            expect(location.path()).toBe('/remove_attribute/ITEM0001');
+        it('should be able to go home', function () {
+            scope.goHome();
+            expect(location.path()).toBe('/');
         });
 
         it('should be able to go back', function () {
             $httpBackend.flush();
-            scope.cancel();
+            scope.goBack();
             expect(location.path()).toBe('/detail/ITEM0001');
+        });
+
+        it('should be able to save attribute', function () {
+            $httpBackend.flush();
+            $httpBackend.expectPOST('/api/item/ITEM0001');
+            scope.attr = {
+                name: 'Trotyl',
+                val: 'HeHe'
+            };
+            scope.saveIt();
         });
 
     });
