3e21323b3f55599bd998daf6ef4f3d20486da90a
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-26 14:39:55 +0800

    添加前端页面 后台折扣规则页

---
+++ public/backend/rule/ruleController.js
@@ -0,0 +1,15 @@
+posManager.controller('RuleController', ['$scope', '$location', '$route', '$routeParams', 'Item',
+    function($scope, $location, $route, $routeParams, Item) {
+
+        var initialize = function () {
+
+        };
+        initialize();
+
+
+    }]);
+
+
+
+
+
