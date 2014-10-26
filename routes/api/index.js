c2d815b9775f9f2079b058fc00b25f959b471bbc
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-26 15:23:31 +0800

    增加后台路由 restful rule

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var item = require('./item');
+var rule = require('./rule');
 var pos = require('./pos');
 
 /* GET users listing. */
@@ -10,6 +11,7 @@ router.get('/', function(req, res) {
 });
 
 router.use('/item', item);
+router.use('/rule', rule);
 router.use('/pos', pos);
 
 module.exports = router;
---
+++ routes/api/rule.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var Storage = require('../../models/storage');
+
+router.get('/', function (req, res) {
+    res.json('');
+});
+
+router.post('/', function (req, res) {
+    var rule = req.body;
+    res.json('');
+});
+
+module.exports = router;
