d6f5d17b4a49170665137cbc7fa5434e99448045
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-26 13:13:10 +0800

    增加前端控制器 前台付款页

--- public/frontend/payment/paymentController.js
+++ public/frontend/payment/paymentController.js
@@ -1,6 +1,24 @@
 posApp.controller('PaymentController', ['$scope', '$location', '$route', '$routeParams', 'Cart',
     function($scope, $location, $route, $routeParams, Cart) {
+        var initialize = function () {
+            $scope.items = Cart.get();
+            $scope.gifts = _($scope.items).where({ promotion: true });
+            $scope.total = _($scope.items).reduce(function (sum, item) {
+                return item.price * item.amount + sum;
+            }, 0);
+        };
+        initialize();
 
+        $scope.pay = function () {
+            Cart.pay(function (err) {
+                if(err) {
+                    alert(err);
+                }
+                else {
+                    $scope.goHome();
+                }
+            });
+        }
     }]);
 
 
