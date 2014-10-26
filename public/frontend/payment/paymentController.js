f1d8c620273171e0a388bd8e35ee1e057ee5290c
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-26 13:26:16 +0800

    修改前端页面 付款页

--- public/frontend/payment/paymentController.js
+++ public/frontend/payment/paymentController.js
@@ -2,9 +2,12 @@ posApp.controller('PaymentController', ['$scope', '$location', '$route', '$route
     function($scope, $location, $route, $routeParams, Cart) {
         var initialize = function () {
             $scope.items = Cart.get();
-            $scope.gifts = _($scope.items).where({ promotion: true });
+            $scope.gifts = _($scope.items).where({ promotion: true }).value() || [];
             $scope.total = _($scope.items).reduce(function (sum, item) {
                 return item.price * item.amount + sum;
+            }, 0);
+            $scope.saving = _($scope.gifts).reduce(function (sum, item) {
+                return 0;
             }, 0);
         };
         initialize();
