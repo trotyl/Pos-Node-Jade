e4111dd15baa5c08236fb5c3bae0fa1c0793a574
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-26 14:30:54 +0800

    增加前端页面 后台折扣活动页

---
+++ public/backend/promotion/promotionController.js
@@ -0,0 +1,11 @@
+posManager.controller('PromotionController', ['$scope', '$location', '$route', '$routeParams', 'Item',
+    function($scope, $location, $route, $routeParams, Item) {
+
+        var initialize = function () {
+
+        };
+        initialize();
+
+    }]);
+
+
